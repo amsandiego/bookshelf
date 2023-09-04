f2dc24d7e49e74cf39519e93cf1ab48d7fbc6ec6
Annamarie San Diego <annamarie.sandiego@gmail.com>
2023-09-04 17:44:34 +0800

    Added logic for saving input data to local storage

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+class Book {
+  constructor(title, author, status) {
+    this.title = title;
+    this.author = author;
+    this.status = status;
+    this.id = Math.random().toString(10).slice(2);
+  }
+}
+
+class Library {
+  constructor() {
+    this.bookshelf = [];
+  }
+
+  addBook(book) {
+    this.bookshelf.push(book);
+  }
+}
+
+const library = new Library();
+
+const addBookToBookshelf = (e) => {
+  e.preventDefault();
+  const newBook = getInputData();
+
+  library.addBook(newBook);
+  saveData();
+  closeAddBookModal();
+};
+
+const getInputData = () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const status = document.getElementById('status').value;
+
+  return new Book(title, author, status);
+};
+
+// Local storage
+const saveData = () => {
+  localStorage.setItem('bookshelf', JSON.stringify(library.bookshelf));
+};
+
 // User Interface
 const addBookBtn = document.getElementById('add-book');
 const addBookModal = document.getElementById('add-book-modal');
@@ -13,14 +56,14 @@ const openAddBookModal = () => {
 const closeAddBookModal = () => {
   overlay.classList.remove('active');
   addBookModal.classList.remove('active');
-  errorMsg.classList.remove('active');
-  errorMsg.textContent = '';
 };
 
 const handleKeyboardInput = (e) => {
   if (e.key === 'Escape') closeAddBookModal();
 };
 
+// Event listeners
 window.onkeydown = handleKeyboardInput;
 addBookBtn.onclick = openAddBookModal;
 overlay.onclick = closeAddBookModal;
+addBookForm.onsubmit = addBookToBookshelf;
